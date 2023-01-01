9892f4fed177985d9b01416f7f6b8f453414b256
only1Migwi <charliemigwi@gmail.com>
2023-01-01 08:44:36 +0300

    admin site customization

--- quote/admin.py
+++ quote/admin.py
@@ -8,12 +8,38 @@ class QuoteAdmin(admin.ModelAdmin):
     # search_fields = ('id','owner','created_at','updated_at')
     ordering = ('-created_at',)
 
+class Quote_AirAdmin(admin.ModelAdmin):
+    list_display = ('id','quote_type','quote_serial_no', 'owner', 'incoterm', 'collection_address', 'cargo_weight') 
+    list_display_links =('id', 'quote_serial_no', 'quote_type', 'owner')
+    list_filter = ('quote_serial_no', 'owner')
+
+class Quote_SeaAdmin(admin.ModelAdmin):
+    list_display = ('id','quote_type','quote_serial_no', 'owner', 'incoterm', 'container_size') 
+    list_display_links =('id', 'quote_serial_no', 'quote_type', 'owner')
+    list_filter = ('quote_serial_no', 'owner')
+
+class Quote_RoadAdmin(admin.ModelAdmin):
+    list_display = ('id','quote_type','quote_serial_no', 'owner', 'incoterm') 
+    list_display_links =('id', 'quote_serial_no', 'quote_type', 'owner')
+    list_filter = ('quote_serial_no', 'owner')
+
+
+class Quote_WarehouseAdmin(admin.ModelAdmin):
+    list_display = ('id','quote_serial_no', 'owner') 
+    list_display_links =('id', 'quote_serial_no','owner')
+    list_filter = ('quote_serial_no', 'owner')
+
+class Quote_AppAdmin(admin.ModelAdmin):
+    list_display = ('id', 'quote_serial_no','quote_type', 'owner', 'status')
+    list_display_links = ('id', 'quote_serial_no','quote_type' )
+    list_filter = ('quote_serial_no','quote_type', 'owner')
+
 admin.site.register(Quote,QuoteAdmin)
-admin.site.register(Quote_Air)
-admin.site.register(Quote_Sea)
-admin.site.register(Quote_Road)
-admin.site.register(Quote_Warehouse)
+admin.site.register(Quote_Air,Quote_AirAdmin)
+admin.site.register(Quote_Sea,Quote_SeaAdmin)
+admin.site.register(Quote_Road,Quote_RoadAdmin)
+admin.site.register(Quote_Warehouse,Quote_WarehouseAdmin)
 admin.site.register(QuoteType)
-admin.site.register(Quote_App)
+admin.site.register(Quote_App, Quote_AppAdmin)
 admin.site.register(Staff_Pricing_Quotation)
 admin.site.register(Taxes)
